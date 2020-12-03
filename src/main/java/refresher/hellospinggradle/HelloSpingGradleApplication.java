2a0ce7fdebf218e659438a4bfd5d6bf65ee52acf
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-02 21:00:12 -0600

    init set up for spring practice

---
+++ src/main/java/refresher/hellospinggradle/HelloSpingGradleApplication.java
@@ -0,0 +1,13 @@
+package refresher.hellospinggradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpingGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpingGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/refresher/hellospinggradle/HelloSpingGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package refresher.hellospinggradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpingGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
