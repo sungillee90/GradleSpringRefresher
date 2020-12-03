39ab16fe4f0f86d197da8915840f8cf3889de128
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-03 09:48:07 -0600

    added templateVC

--- src/main/java/refresher/hellospinggradle/controller/HelloController.java
+++ src/main/java/refresher/hellospinggradle/controller/HelloController.java
@@ -3,12 +3,20 @@ package refresher.hellospinggradle.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
     @GetMapping("hello")
     public String hello(Model m) {
         m.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model m) {
+        m.addAttribute("name", name);
+        return "hello-template";
     }
 }
