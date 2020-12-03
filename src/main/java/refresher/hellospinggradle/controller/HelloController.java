f9e8942054ba459ba2e137fce42f76e50ddb33cc
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-03 10:25:44 -0600

    added ResponseBody view

--- src/main/java/refresher/hellospinggradle/controller/HelloController.java
+++ src/main/java/refresher/hellospinggradle/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -15,8 +16,14 @@ public class HelloController {
     }
 
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model m) {
+    public String helloMvc(@RequestParam(value = "name") String name, Model m) {
         m.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloMvc(@RequestParam(value = "name") String name) {
+        return "hello " + name;
     }
 }
