e506c1fcb71654308243e656d8ee09e1946b5933
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-03 10:28:56 -0600

    added API JSON data

--- src/main/java/refresher/hellospinggradle/controller/HelloController.java
+++ src/main/java/refresher/hellospinggradle/controller/HelloController.java
@@ -26,4 +26,23 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name") String name) {
         return "hello " + name;
     }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public  Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
