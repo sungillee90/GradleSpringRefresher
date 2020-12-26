4bb84c77d135b55aaee2cd1e60ebdfe9fcc4c92f
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-26 09:57:25 -0600

    WIP trying to connect View and Controller

--- src/main/java/refresher/hellospinggradle/controller/NewYearController.java
+++ src/main/java/refresher/hellospinggradle/controller/NewYearController.java
@@ -1,4 +1,17 @@
 package refresher.hellospinggradle.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class NewYearController {
+    @GetMapping("newYearProject")
+    @ResponseBody
+    public HelloController.Hello helloApi(@RequestParam("name") String name) {
+        HelloController.Hello hello = new HelloController.Hello();
+        hello.setName(name);
+        return hello;
+    }
 }
